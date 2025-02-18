cf7e4af59e7d52de600e944f8f236090d96e8c66
Ruan <ruan.oliveiralopes@hotmail.com>
2025-02-18 02:48:03 -0300

    update

--- README.md
+++ README.md
@@ -13,17 +13,18 @@ hyprland        (windows manager)
 
 ## Colours
 
-<pre>
-Black         #222831
-Red           #F4A3A3
-Green         #D4E3A8
-Yellow        #E8BC7A
-Blue          #B8E5E7
-Purple        #9582B7
-Cyan          #52D6D3
-White         #FAF0E7
+| color# |Base       |Darker     |Lighter    |Label      |
+|:------:|:---------:|:---------:|:---------:|:---------:|
+| color0 | #222831 | #161C22 | #2E3640 | Black     |
+| color1 | #F4A3A3 | #C07C7C | #FFB8B8 | Red       |
+| color2 | #D4E3A8 | #A8BF80 | #E2F0BE | Green     |
+| color3 | #E8BC7A | #B9935F | #F2CC96 | Yellow    |
+| color4 | #B8E5E7 | #8CB8BA | #C7F1F2 | Blue      |
+| color5 | #9582B7 | #6F5A91 | #A994CC | Purple    |
+| color6 | #52D6D3 | #3BA8A6 | #7BE7E5 | Cyan      |
+| color7 | #FAF0E7 | #D8C6B6 | #FFF8F0 | White     |
+| colorG | #E5B566 | #B8914E | #F2C984 | Gold      |
+| colorB | #17121C | #140f1b | #2A2136 | Dark purpl|     
+| colorR | #F53C3C | null      | null      | Light Red |
+| colorG | #606060 | null      | null      | Grey      |
 
-Goldish       #E5B566
-Dark Blue     #17121C
-Lightish Red  #F53C3C
-</pre>
--- update.sh
+++ update.sh
@@ -6,56 +6,58 @@ fi
 USER_HOME=$(eval echo ~$SUDO_USER)
 SUCCESS="(˶ᵔ ᵕ ᵔ˶)"
 
-if [ ! -d "./.config" ]; then
+if [ ! -d "./config" ]; then
 	printf "Where did the repo's .config file go (ó﹏ò｡)\nCreating a new one ദ്ദി( • ᴗ - ) ✧\n"
 	mkdir .config
 fi
 
+chown -R 777 ./config
+
 # Hyprland dotfiles (Hyprland itself and widgets/components/utilities)
 
 if [ -d "$USER_HOME/.config/hypr" ]; then
-	cp -r "$USER_HOME/.config/hypr/" ./.config
+	cp -r "$USER_HOME/.config/hypr/" ./config
 	echo "$SUCCESS Hypr updated"
 else
 	echo "Could not find hypr folder"
 fi
 
 if [ -d "$USER_HOME/.config/waybar" ]; then
-	cp -r "$USER_HOME/.config/waybar" ./.config
+	cp -r "$USER_HOME/.config/waybar" ./config
 	echo "$SUCCESS Waybar updated"
 fi
 
 # Terminal dotfiles (For terminal stuff like kitty, starship, neofetch)
 
 if [ -d "$USER_HOME/.config/kitty" ]; then
-	cp -r "$USER_HOME/.config/kitty/" ./.config
+	cp -r "$USER_HOME/.config/kitty/" ./config
 	echo "$SUCCESS Kitty updated"
 else
 	echo "Could not find kitty folder"
 fi
 
 if [ -f "$USER_HOME/.config/starship.toml" ]; then
-	cp "$USER_HOME/.config/starship.toml" ./.config
+	cp "$USER_HOME/.config/starship.toml" ./config
 else
 	echo "Could not find starship file"
 fi
 
 if [ -d "$USER_HOME/.config/fastfetch/" ]; then
-	cp -r "$USER_HOME/.config/fastfetch/" ./.config
+	cp -r "$USER_HOME/.config/fastfetch/" ./config
 	echo "$SUCCESS Fastfetch updated"
 else
 	echo "Could not find fastfetch folder"
 fi
 
 if [ -d "$USER_HOME/.config/eww/" ]; then
-	cp -r "$USER_HOME/.config/eww" ./.config
+	cp -r "$USER_HOME/.config/eww" ./config
 	echo "$SUCCESS Eww updated"
 else
 	echo "Could not find eww folder"
 fi
 
 if [ -d "$USER_HOME/.config/rofi/" ]; then
-	cp -r "$USER_HOME/.config/rofi" ./.config
+	cp -r "$USER_HOME/.config/rofi" ./config
 	echo "$SUCCESS Rofi updated"
 else
 	echo "Could not find rofi folder"
@@ -63,5 +65,4 @@ fi
 
 # Changes ownership to YOU. Yes, you. ദ്ദി( • ᴗ - ) ✧
 
-chown -R $(whoami):$(whoami) ./.config
-chmod -R u+rw ./.config
+chown -R 777 ./config
